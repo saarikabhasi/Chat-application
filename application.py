3d4753dacdbc744deffa265519297789c49b6f1c
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2020-06-20 23:20:55 -0400

    channel specific message, socket working

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 import os 
-from flask import Flask,render_template,request,redirect,url_for,jsonify,flash
+from flask import Flask,session,render_template,request,redirect,url_for,jsonify,flash
 from flask_socketio import SocketIO, emit,send
+from flask_session import Session
 
 app = Flask(__name__)
 # app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
@@ -8,58 +9,143 @@ app.config["SECRET_KEY"] = "my secret key"
 #socketio = SocketIO(app)
 socketio = SocketIO(app,engineio_logger=True)
 
+channels=[]
 
+channelmessages = dict()
 @app.route("/")
 def index():
-  return render_template ("index.html")
+  if 'name' in session:
+     if session['name']:
+       message = session['name']
+       print("All channels index",channels)
+       return render_template ("index.html",allchannels=channels,account = message)
+  else:
+    return redirect(url_for('displayname'))
 
-#@app.route("/createchannel/<string:name>",methods=["GET","POST"])
-@app.route("/createname",methods=["GET","POST"])
-def createname():
-  
-  name = request.form["name"]
-  print("created name" ,f"{name}")
-  print("Method",request.method)
-  #return redirect(url_for('createchannel',name=name))  
-  return render_template("channel.html",name=name)
-  #return redirect(url_for('index'))
+@app.route("/displayname",methods=["GET","POST"])
+def displayname():
+  if 'name' in session:
+     if session['name']:
+       print("Displayname log1")
+       message = session['name']
+       return render_template ("index.html",account = message)
 
-allchannels=[]
+  print(" Displayname Request method",request.method)
+
+  if request.method == 'POST':
+    try:
+      name = request.form["name"]
+    except ValueError:
+      return render_template("login.html",err_msg="Display name is required to begin chat")
+
+    if len(name) == 0:
+      return render_template("login.html",err_msg="Display name is required to begin chat")
+    else:
+      session['name']= name 
+      print(" Displayname created name" ,f"{name}")
+      return redirect(url_for('index'))  
+  else:
+    return render_template("login.html")
+
+
+
 @app.route("/createchannel",methods=["GET","POST"])
 def createchannel():
+  if 'name' in session:
+     if session['name']:
+       message =session['name']
+      
   error_msg=""
   print("in createchannel")
   #name = request.args['name']
   # print("name is:",name)
-  print(request.method)
+  print("createchannel",request.method)
   if request.method == "POST":
     print("here")
     
     channelname = request.form['cname']
-    print("create new channel")
-    print("all channels",allchannels)
-    if channelname in allchannels:
+    print("createchannel, create new channel")
+    print("createchannel ,all channels",channels)
+    channelname = channelname.lower()
+    if channelname in channels:
       error_msg="channel name is already taken"
     else:
-      allchannels.append(channelname)
-      print("all channels after appending",allchannels)
-    return render_template("channel.html", error_msg=error_msg, allchannels=allchannels)
+      channels.append(channelname)
+      
+      print("createchannel, all channels after appending",channels)
+    return render_template("index.html", error_msg=error_msg, account = message,allchannels=channels)
   else:
-    return render_template("channel.html", allchannels=allchannels)
+    return render_template("index.html", account = message,allchannels=channels)
 
-  # allmessages=[]
-  # @socketio.on('send message')
-  # def chat(data):
-  #   print("in chat")
-  #   message = data["message"]
-  #   print("messages are :",message)
-  #   allmessages.append(message)
-  #   emit("announce message", {"message": message}, broadcast=True)
-  # allmessages=[]
-  @socketio.on('send message')
-  def chat(message):
-    print("in chat")
-    print("messages are :",message)
-    # allmessages.append(message)
-    emit('announce chat', {"message": message}, broadcast=True)
+@app.route("/search",methods=["GET","POST"])
+def search():
+  if session['name']:
+      message = session['name']
+  else:
+      return render_template("login.html",account="you are not logged in")
 
+  if request.method == 'POST':     
+    query = request.form["search"]
+    
+    #empty query
+    if query == "":
+      No_Results ="No results"
+      return render_template("index.html",search_error = No_Results, account=message)
+
+    else:
+      query=query.strip()
+      searchquery = query.lower()
+
+      if searchquery in channels:
+        return render_template("index.html",searchresults = searchquery,allchannels=channels,account=message )
+      else:
+        No_Results ='No channels found'
+        return render_template("index.html", search_error = No_Results,searchresults = searchquery,account=message )
+  else:
+    return redirect(url_for('index'))
+  
+@app.route("/channel/<string:channelname>",methods=["GET","POST"])
+def channel(channelname):
+  if session['name']:
+      message = session['name']
+  else:
+      return render_template("login.html",account="you are not logged in")
+
+
+  session['currentchannel'] = channelname
+  print("channelname ", channelmessages)
+  return render_template("channel.html",channelname = channelname,channelmessages =channelmessages , account=message)
+@app.route("/logout",methods=["GET","POST"])
+def logout():
+  session.pop('name',None)
+  return redirect(url_for('index'))
+
+@socketio.on('send message')
+def chat(message,timestamp):
+  username = session['name']
+  channelname = session['currentchannel']
+  print("in chat")
+  print("message is :",message)
+  print("time is:",timestamp)
+  messagedetail = [username,message,timestamp]
+ 
+  print("list is :",messagedetail)
+  if channelname not in channelmessages:
+      channelmessages[channelname]=list()
+   
+
+  channelmessages[channelname].append(messagedetail)
+  
+
+  print(" channel messages:",channelmessages)
+  emit('announce message', {"username":username,"message": message,"timestamp":timestamp}, broadcast=True)
+
+# {
+#  "general": [
+#             {
+#               "A": ["00:01","hi"],
+#               "B": ["00:03","hello"],
+#                "A": ["00:3","good"]
+#           }
+#           ]
+# }
--- static/channel.js
+++ static/channel.js
@@ -1,36 +1,64 @@
-document.addEventListener('DOMContentLoaded', () => {
-    // Connect to websocket
-  var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-  //var socket = io.connect(location.origin );
-    console.log(socket);
-    // When connected, configure buttons
-      socket.on('connect', () => {
-        socket.emit('joined');
+
+    document.addEventListener('DOMContentLoaded', () => {
+        // Connect to websocket
+        var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+        console.log(socket);
+
+        // When connected, configure buttons
+        socket.on('connect', () => {
+        socket.emit('joined');     
+        // Each button should emit a "submit msg" event
+        //document.querySelector('#sendmsg').button.onclick = () => {
+        document.querySelector('#sendbutton').addEventListener('click', () => {
+        console.log(document.getElementById("writemessageID").value);
+        let message = document.getElementById("writemessageID").value;
+        let currenttime = new Date();
+        let timestamp = currenttime.toUTCString()
+        console.log("time",timestamp);
+        console.log(timestamp)  
+        console.log("message in #sendbutton",message);
+        console.log("socket in #sendbutton",socket);
         
-          // Each button should emit a "submit msg" event
-          //document.querySelector('#sendmsg').button.onclick = () => {
-            document.querySelector('#sendmsg').addEventListener('click', () => {
-                  console.log(document.getElementById("messagebox").value);
-                  let message = document.getElementById("messagebox").value;
-                  console.log(message);
-                  console.log(socket);
-                  socket.emit('send message' ,message);
-                  document.getElementById("messagebox").value = '';
-                  // socket.emit('hello', 'can you hear me?', 1, 2, 'abc');
-              }
-          );
+        socket.emit('send message' ,message,timestamp);
+        //addmsg = (message)=>{
+          // var newmessageDiv = document.createElement('div');
+          // newmessageDiv.className ="container--msg"
+          // container-new-msg.innerHTML = message + currenttime + timestamp;
+          
+        //}
+        document.getElementById("writemessageID").value = '';
+        
+        // socket.emit('hello', 'can you hear me?', 1, 2, 'abc');
         });
+    });
+    // When a new msg is announced
+    socket.on('announce message', data => {
+        // var msg = data.message;
+        console.log("in announce data",data);
+        values =  `${data.username} ${data.message} ${data.timestamp}`
+       
+        console.log("in announce values",values);
+        //console.log("displaychanneltextarea",document.querySelector('#displaychannelmessagesID').value)
+        //document.querySelector('#displaychannelmessagesID').value += row + '\n'
 
-      // When a new msg is announced
-      socket.on('announce chat', data => {
-          // var msg = data.message;
-          let row =  `${data.message}`
-          console.log(row);
-          document.querySelector('#mesg').value += row + '\n'
+        //console.log(document.getElementById('displayallmessagesID').getAttribute('value'))
+        //var newmessageDiv = document.createElement('div'),
+        //newmessageDiv = document.getElementById("container-new-msg");
         
+        //var newmessageDiv = getElementsById('container-new-msg');
+        if(document.getElementById('container-msg') != null){
 
-      });
+             document.getElementById('container-msg').innerHTML += values+'\n';
+        }  
+        else{
+            document.getElementById('container-msg').innerHTML =  values+'\n';
+       
+        }
+
+    });
       
-
+  //oldmessage 
+  
     
   });
+
