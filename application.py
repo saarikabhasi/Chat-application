bb5d5f1eab70b2f179fd7229dbc2a17778cf5104
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2020-06-22 19:43:08 -0400

    store last channel, styling changes for messages, store maximum of 100 messages

--- application.py
+++ application.py
@@ -1,36 +1,38 @@
 import os 
+from collections import deque
 from flask import Flask,session,render_template,request,redirect,url_for,jsonify,flash
 from flask_socketio import SocketIO, emit,send
 from flask_session import Session
 
+
 app = Flask(__name__)
 # app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SECRET_KEY"] = "my secret key"
-#socketio = SocketIO(app)
-socketio = SocketIO(app,engineio_logger=True)
+socketio = SocketIO(app)
+# socketio = SocketIO(app,engineio_logger=True)
 
 channels=[]
 
 channelmessages = dict()
+
 @app.route("/")
 def index():
   if 'name' in session:
      if session['name']:
        message = session['name']
-       print("All channels index",channels)
        return render_template ("index.html",allchannels=channels,account = message)
   else:
     return redirect(url_for('displayname'))
 
+#get display name
 @app.route("/displayname",methods=["GET","POST"])
 def displayname():
+
+  #making sure if user has logged in
   if 'name' in session:
      if session['name']:
-       print("Displayname log1")
        message = session['name']
        return render_template ("index.html",account = message)
-
-  print(" Displayname Request method",request.method)
 
   if request.method == 'POST':
     try:
@@ -38,114 +40,132 @@ def displayname():
     except ValueError:
       return render_template("login.html",err_msg="Display name is required to begin chat")
 
+    #if name length is zero
     if len(name) == 0:
       return render_template("login.html",err_msg="Display name is required to begin chat")
+
     else:
+
+      #set display name
       session['name']= name 
-      print(" Displayname created name" ,f"{name}")
       return redirect(url_for('index'))  
   else:
     return render_template("login.html")
 
 
-
+#create a channel
 @app.route("/createchannel",methods=["GET","POST"])
 def createchannel():
+
+  #making sure if user has logged in
   if 'name' in session:
      if session['name']:
        message =session['name']
       
   error_msg=""
-  print("in createchannel")
-  #name = request.args['name']
-  # print("name is:",name)
-  print("createchannel",request.method)
+
   if request.method == "POST":
-    print("here")
-    
+
+    #create
     channelname = request.form['cname']
-    print("createchannel, create new channel")
-    print("createchannel ,all channels",channels)
     channelname = channelname.lower()
+
+    # making sure new channelname is not already taken
     if channelname in channels:
       error_msg="channel name is already taken"
     else:
       channels.append(channelname)
       
-      print("createchannel, all channels after appending",channels)
     return render_template("index.html", error_msg=error_msg, account = message,allchannels=channels)
+
   else:
     return render_template("index.html", account = message,allchannels=channels)
 
+#search
 @app.route("/search",methods=["GET","POST"])
 def search():
+
+  #make sure if user has logged in
   if session['name']:
       message = session['name']
   else:
       return render_template("login.html",account="you are not logged in")
 
-  if request.method == 'POST':     
+  if request.method == 'POST':   
+
+    #search for channel 
     query = request.form["search"]
     
     #empty query
     if query == "":
       No_Results ="No results"
-      return render_template("index.html",search_error = No_Results, account=message)
+      return render_template("index.html",search_error = No_Results, allchannels=channels,account=message)
 
     else:
+      #strip white space
       query=query.strip()
+
+      #transform to lower case as channel names are stored in lower case
       searchquery = query.lower()
 
       if searchquery in channels:
+        #send results if found
+
         return render_template("index.html",searchresults = searchquery,allchannels=channels,account=message )
+      
       else:
+        #send 'no channels found' if not found
+
         No_Results ='No channels found'
-        return render_template("index.html", search_error = No_Results,searchresults = searchquery,account=message )
+        return render_template("index.html", search_error = No_Results,searchresults = searchquery,allchannels=channels,account=message )
   else:
     return redirect(url_for('index'))
-  
+
+
+#current channel  
 @app.route("/channel/<string:channelname>",methods=["GET","POST"])
 def channel(channelname):
+
+  #make sure if user has logged in
   if session['name']:
       message = session['name']
   else:
       return render_template("login.html",account="you are not logged in")
 
-
+  #setting currentchannel
   session['currentchannel'] = channelname
-  print("channelname ", channelmessages)
+
+
   return render_template("channel.html",channelname = channelname,channelmessages =channelmessages , account=message)
+
+#logout
 @app.route("/logout",methods=["GET","POST"])
 def logout():
   session.pop('name',None)
   return redirect(url_for('index'))
 
+#send message
 @socketio.on('send message')
-def chat(message,timestamp):
+def chat(message,time,date,attachimg,attachdoc):
+
+  #retreive information from socket emit. 
+  
   username = session['name']
   channelname = session['currentchannel']
-  print("in chat")
-  print("message is :",message)
-  print("time is:",timestamp)
-  messagedetail = [username,message,timestamp]
+  attachment = [attachimg,attachdoc]
+  messagedetail = [username,message,time,date,attachment]
  
-  print("list is :",messagedetail)
+  #store channel messages
   if channelname not in channelmessages:
-      channelmessages[channelname]=list()
-   
+      channelmessages[channelname] = deque()
 
+  # if channel message more than 100, remove old message
+  if len(channelmessages[channelname]) >=100:
+    channelmessages[channelname].popleft()
+
+  #append new message
   channelmessages[channelname].append(messagedetail)
   
+  
+  emit('announce message', {"username":username,"message": message,"time":time, "date":date,"currentchannel":channelname,"attachment":[attachimg, attachdoc]}, broadcast=True)
 
-  print(" channel messages:",channelmessages)
-  emit('announce message', {"username":username,"message": message,"timestamp":timestamp}, broadcast=True)
-
-# {
-#  "general": [
-#             {
-#               "A": ["00:01","hi"],
-#               "B": ["00:03","hello"],
-#                "A": ["00:3","good"]
-#           }
-#           ]
-# }
--- static/channel.js
+++ static/channel.js
@@ -1,103 +1,152 @@
-
     document.addEventListener('DOMContentLoaded', () => {
+
         // Connect to websocket
         var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-        console.log(socket);
+        
 
-        // When connected, configure buttons
+        //socket connect
         socket.on('connect', () => {
-        socket.emit('joined');     
-        // Each button should emit a "submit msg" event
-        //document.querySelector('#sendmsg').button.onclick = () => {
-        document.querySelector('#sendbutton').addEventListener('click', () => {
-        console.log(document.getElementById("writemessageID").value);
-        let message = document.getElementById("writemessageID").value;
-        let currenttime = new Date();
-        let timestamp = currenttime.toUTCString()
-        console.log("time",timestamp);
-        console.log(timestamp)  
-        console.log("message in #sendbutton",message);
-        console.log("socket in #sendbutton",socket);
+            console.log("socket connect")
+            socket.emit('joined');     
+            console.log("socket after join")
+            document.querySelector('#sendbutton').addEventListener('click', () => {
+                
+                let message = document.getElementById("writemessageID").value;
+                let currenttime = new Date();
+                
+                time = ` ${currenttime.toLocaleTimeString() }`;
+                date = `  ${currenttime.toLocaleDateString()} `;
+                console.log("send attachments",document.getElementById('img').value,document.getElementById('doc').value)
+                if (document.getElementById('img').value){
+                    attachimg= document.getElementById('img').value.replace("C:\\fakepath\\", "");
+                }
+                else{ attachimg= ""}
+                if (document.getElementById('doc').value){
+                    attachdoc= document.getElementById('doc').value.replace("C:\\fakepath\\", "");
+                }
+                else{
+                    attachdoc =""
+                }
+                
+                   
+                    
+                
+                
+
+                console.log("Send",attachimg,attachdoc)
+                socket.emit('send message' ,message,time,date,attachimg,attachdoc);
+
+                document.getElementById("writemessageID").value = '';
         
-        socket.emit('send message' ,message,timestamp);
-        //addmsg = (message)=>{
-          // var newmessageDiv = document.createElement('div');
-          // newmessageDiv.className ="container--msg"
-          // container-new-msg.innerHTML = message + currenttime + timestamp;
-          
-        //}
-        document.getElementById("writemessageID").value = '';
-        
-        // socket.emit('hello', 'can you hear me?', 1, 2, 'abc');
+       
+            });
         });
-    });
+
     // When a new msg is announced
-    socket.on('announce message', data => {
-        // var msg = data.message;
-        console.log("in announce data",data);
-        values =  `${data.username} ${data.message} ${data.timestamp}`
-       
-        console.log("in announce values",values);
-        //console.log("displaychanneltextarea",document.querySelector('#displaychannelmessagesID').value)
-        //document.querySelector('#displaychannelmessagesID').value += row + '\n'
 
-        //console.log(document.getElementById('displayallmessagesID').getAttribute('value'))
-        //var newmessageDiv = document.createElement('div'),
-        //newmessageDiv = document.getElementById("container-new-msg");
+        socket.on('announce message', data => {
+
+            //set current channel
+            // console.log("currentchannel",localStorage.getItem('currentchannel'))
+            if  (localStorage.getItem('currentchannel')){
+                localStorage.removeItem('currentchannel');
+            }
         
-        //var newmessageDiv = getElementsById('container-new-msg');
-        // if(document.getElementById('container-msg') != null){
+            localStorage.setItem('currentchannel', data.currentchannel);
+            console.log("current channel", localStorage.getItem('currentchannel'))
 
-        //      document.getElementById('container-msg').innerHTML += values+'\n';
-        // }  
-        // else{
-        //     document.getElementById('container-msg').innerHTML =  values+'\n';
+            //create new div for displaying new messages
+            let newDiv = document.createElement('div');
+            
+            // place new div to right if current user is sending message
+            if (data.username == localStorage.getItem('name')){
+                newDiv.className = 'containers container-right';
+            }
+            //place new div to left if other users are sending message
+            else{
+                newDiv.className = 'containers container-left ';
+            }
+
+            
+            // creating other elements of new messages div
+            let newP1 = document.createElement('h2');
+            newP1.innerHTML = data.username;
+
+            let newP2 = document.createElement('h3');
+            newP2.className = 'lead text-secondary';
+            newP2.innerHTML =  data.message;
+
+            let newP3 = document.createElement('p');
+            newP3.className = 'lead';
+            newP3.innerHTML = data.time;
+
+            let newP4 = document.createElement('p');
+            newP4.className = 'lead';
+            newP4.innerHTML = data.date;
+            console.log("attachments",data.attachment)
+
+            if (data.attachment){
+                console.log(data.attachment[0],data.attachment[1])
+                if (data.attachment[0]){
+                    var extension = data.attachment[0].split('.').pop();
+                    if (extension == "jpg") {
+                        var newP5 = document.createElement('img');
+                        let attachimg = data.attachment[0].replace("C:\\fakepath\\", "");
+                        
+                        console.log("A",attachimg)
+                        newP5.alt = attachimg
+                        newP5.width="100"
+                        newP5.height="100"
+                        // newP5.className = 'lead float-right';
+                        newP5.innerHTML = attachimg;
+                    }
+                }
+                if (data.attachment[1]){
+                    var extension = data.attachment[1].split('.').pop();
+                    if (extension === "pdf" || extension === "doc" || extension === "docx") {
+                        var newP6 = document.createElement('p');
+                        let attachdoc = data.attachment[1].replace("C:\\fakepath\\", "");
+                        
+                        newP6.className = 'lead';
+                        newP6.innerHTML = attachdoc ;
+                    }
+                
+                }
+            }
+               
+
+            newDiv.appendChild(newP1);
+            newDiv.appendChild(newP2);
+            newDiv.appendChild(newP3);
+            newDiv.appendChild(newP4);
+            if (newP5){
+                console.log("P5",newP5)
+                newDiv.appendChild(newP5);
+            }
+            if (newP6){
+                console.log("P6",newP6)
+                newDiv.appendChild(newP6);
+            }
+        
+        
        
-        // }
-
-        let newDiv = document.createElement('div');
-        console.log(localStorage.getItem('name'));
-        if (data.username == localStorage.getItem('name')){
-            newDiv.className = 'containers container-right float-right';
-        }
-        else{
-            newDiv.className = 'containers container-left float-left';
-        }
-       
-
-        let newP1 = document.createElement('h5');
-        newP1.innerHTML = data.username;
-
-        let newP2 = document.createElement('p');
-        newP2.className = 'lead text-secondary';
-        newP2.innerHTML =  data.message;
-
-        let newP3 = document.createElement('p');
-        newP3.className = 'lead text-secondary';
-        newP3.innerHTML = data.timestamp;
-
-        newDiv.appendChild(newP1);
-        newDiv.appendChild(newP2);
-        newDiv.appendChild(newP3);
-        //document.body.appendChild(newDiv);
-        console.log(newDiv);
-       // var x = document.getElementsByClassName("container-msg");
         
         let length = document.getElementsByClassName("container-msg").length;
-        console.log(length);
+        
         if (length == 0){
-            //document.getElementsByClassName("container-msg")[0].appendChild(newDiv);
+            //if first message to this channel
 
-            document.querySelector(".container-msg").innerHTML = newDiv;
+            document.getElementsByClassName("container-msg")[0].appendChild(newDiv);
         }
+        
         else{
+           
             document.getElementsByClassName("container-msg")[length-1].appendChild(newDiv);
         }
         
-        //document.querySelector(".container-msg").appendChild(newDiv);
+
     });
       
-  //oldmessage 
   
     
   });
