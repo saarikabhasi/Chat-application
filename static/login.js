b94dc7e39c4a6302fa68932bf2491a52802a5eb3
Nair Saarika Bhasi <saarikabhasi@yahoo.com>
2020-06-23 12:46:32 -0400

    static/login.js templates/login.html

---
+++ static/login.js
@@ -0,0 +1,26 @@
+// Login
+document.body.style.backgroundColor = 'rgb(' + [0,156,184].join(',') + ')';
+
+if (!localStorage.getItem('name')){
+
+        //if no name found in local storage
+        getname=()=>{
+            let name = document.querySelector('#name').value;
+            
+            localStorage.setItem('name', name);
+            document.getElementById('form').submit();
+        
+            
+        };
+    } 
+else {
+
+    //login with previous display name
+    let name = localStorage.getItem('name')
+    document.querySelector('#name').value = name
+    document.getElementById('form').submit();
+
+}
+
+
+
